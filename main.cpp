102293cf929341db010a349c4522c1054d67a121
Zimnx <mzimnoch@hotmail.com>
2015-04-19 17:11:18 +0200

    Release version

--- main.cpp
+++ main.cpp
@@ -29,13 +29,12 @@ int readMask()
 
 int main()
 {
-    IpPrefixBitmap bitmap, testBitmap;
+    IpPrefixBitmap bitmap;
     while(true)
     {
         int option;
-        std::cout << "\n1.Add\n2.Del\n3.Check\n4.Performance Test\n5.Exit\n>>";
+        std::cout << "\n1.Add\n2.Del\n3.Check\n4.Exit\n>>";
         std::cin  >> option;
-        if(option == 5) break;
         int mask = 0;
         std::vector<unsigned> ip;
         switch(option)
@@ -63,45 +62,7 @@ int main()
             }
             break;
         case 4:
-            using Clock = std::chrono::high_resolution_clock;
-            std::cout << "Performance test: 120'000 operations(30'000 additions, 30'000 deletions, 60'000 checks)\nTime: ";
-            auto t1 = Clock::now();
-            for(int i = 0; i < 100000; ++i)
-            {
-                testBitmap.add(2887060214, 29);
-                testBitmap.add(2887060206, 28);
-                testBitmap.add(2887060206, 21);
-                testBitmap.check(2887060205);
-                testBitmap.del(2887060206, 28);
-                testBitmap.check(2887060205);
-                testBitmap.check(2887060205);
-                testBitmap.del(2887060214, 29);
-                testBitmap.del(2887060206, 21);
-            }
-            auto t2 = Clock::now();
-            std::cout << std::chrono::duration_cast<chrono::milliseconds>(t2 - t1).count() << " milliseconds" << std::endl;
-            std::cout << "Single operation:\nFirst Addition: ";
-            t1 = Clock::now();
-            testBitmap.add(2887060206, 28);
-            t2 = Clock::now();
-            std::cout << std::chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
-            std::cout << "Second Addition: ";
-            t1 = Clock::now();
-            testBitmap.add(2887060203, 27);
-            t2 = Clock::now();
-            std::cout << std::chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
-            std::cout << "Check: ";
-            t1 = Clock::now();
-            testBitmap.check(2887060205);
-            t2 = Clock::now();
-            std::cout << std::chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
-            std::cout << "Deletion: ";
-            t1 = Clock::now();
-            testBitmap.del(2887060206, 28);
-            t2 = Clock::now();
-            std::cout << std::chrono::duration_cast<chrono::nanoseconds>(t2 - t1).count() << " nanoseconds" << std::endl;
-            testBitmap.del(2887060203, 27);
-            break;
+            return 0;
         }
         std::cin.ignore();
     }
